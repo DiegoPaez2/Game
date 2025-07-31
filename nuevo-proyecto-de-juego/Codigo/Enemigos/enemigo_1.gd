95e0fb45b84c53e153789e08ce8fbc874f0dbe92
DiegoPaez2 <diegolexx99@gmail.com>
2025-07-30 20:38:08 -0500

    ¨Enemigo¨

--- nuevo-proyecto-de-juego/Codigo/Enemigos/enemigo_1.gd
+++ nuevo-proyecto-de-juego/Codigo/Enemigos/enemigo_1.gd
@@ -1,60 +1,53 @@
 extends CharacterBody2D
 
+const Personajes_1 = preload("res://Codigo/Personajes/personajes_1.gd")
+
 @export var velocidad := 70
 @export var gravedad := 800
 
-enum Estado { PATRULLANDO, PERSIGUIENDO }
-var estado = Estado.PATRULLANDO
-
 var direccion := 1
+var persiguiendo := false
 var jugador: Node2D = null
 
 @onready var ray_izq = $dect_izq
 @onready var ray_der = $dect_dere
-@onready var area_deteccion = $Area2D
+@onready var anim = $AnimationPlayer
+@onready var sprite = $Sprite2D
 
 func _physics_process(delta):
-	match estado:
-		Estado.PATRULLANDO:
-			patrullar()
-		Estado.PERSIGUIENDO:
-			perseguir()
-
-	if is_on_floor():
-		velocity.y = 0
-	else:
-		velocity.y += gravedad * delta
-
-	move_and_slide()
-
-	$Sprite2D.flip_h = (direccion < 0)
-
-	if velocity.x != 0:
-		$AnimationPlayer.play("Caminar")
-	else:
-		$AnimationPlayer.play("Idle")
-
-func patrullar():
-	if ray_der.is_colliding():
-		direccion = -1
-	elif ray_izq.is_colliding():
-		direccion = 1
-
-	velocity.x = direccion * velocidad
-
-func perseguir():
-	if jugador:
-		direccion = 1 if jugador.global_position.x > global_position.x else -1
+	if persiguiendo and jugador != null:
+		# Seguir al jugador
+		var distancia = jugador.global_position.x - global_position.x
+		direccion = sign(distancia)
 		velocity.x = direccion * velocidad
 	else:
-		estado = Estado.PATRULLANDO
+		# Patrullaje simple
+		if ray_der.is_colliding():
+			direccion = -1
+		elif ray_izq.is_colliding():
+			direccion = 1
+		velocity.x = direccion * velocidad
 
+	# Aplicar gravedad
+	velocity.y += gravedad * delta
+
+	# Movimiento
+	move_and_slide()
+
+	# Animación
+	sprite.flip_h = direccion < 0
+	if abs(velocity.x) > 0:
+		anim.play("Caminar ")
+	else:
+		anim.play("Idle")
+
+# Señales del Area2D para detectar jugador
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.name == "Jugador":
+	if body.name == "Jugador" or body is Personajes_1:
+		persiguiendo = true
 		jugador = body
-		estado = Estado.PERSIGUIENDO
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body == jugador:
+		persiguiendo = false
 		jugador = null
-		estado = Estado.PATRULLANDO
